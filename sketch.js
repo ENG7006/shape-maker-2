3a164897c3d4398a6f483bbb8de829ac083833fc
bazintastic <scr@wayne.edu>
2015-11-05 07:18:51 -0500

    robust-comments
    Add/revise comments to make the thing more pedagogical.

--- sketch.js
+++ sketch.js
@@ -1,27 +1,40 @@
+/*
+	shape-maker:
+	
+	A nicely object-oriented project which interactively draws a shape on the screen.
+	
+	The parameters:
+	* Start with a blank canvas
+	* When the user clicks, begin drawing a shape
+	* Each time the user clicks, add a vertex to the shape
+	* Provide interaction feedback by drawing a line between the most recent vertex and the shape
+	* 
+*/
+
+// I need a holder for a shape
 var s;
 
-// I need a canvas and a ShapeSpace
+// I need a canvas, and to make a shape in the holder
 function setup() {
   createCanvas(600, 600);
   s = new Shape();
-  s.addVertex(300, 300);
-  s.addVertex(350, 350);
-  s.addVertex(100, 400);
 }
 
-// Not sure yet what I need to do here
+// Draw a background, set some conditions, and simply tell the shape to display itself
 function draw() {
   background(0);
   stroke(255);
   fill(100);
-
   s.display();
 }
 
+// When the mouse is clicked, add the current mouse position as a vertex
 function mouseClicked() {
 	s.addVertex(mouseX, mouseY);
 }
 
+// Hit enter: close + print the shape
+// Hit backspace: delete the shape
 function keyPressed() {
 	if (keyCode === ENTER) {
 		s.done();
@@ -34,6 +47,7 @@ function keyPressed() {
 
 
 /*
+  OBJECT: Shape
   A Shape should:
   * display itself
   * add a vertex
@@ -41,7 +55,7 @@ function keyPressed() {
   * output a list of vertices
 */
 
-//Shape constructor: takes either x and y or no parameters.
+//Shape constructor: takes no parameters.
 function Shape() {
   this.vertices = [];
   this.vertexIndex = 0;
@@ -60,8 +74,26 @@ function Shape() {
 	* printVertex: prints an internal vertex object
 	* trackMouse: if the shape is open, draws a line from the last vertex to the current mouse X & Y
 	* 
+	* Uses prototype + "object literal" notation
+	* Every Shape we instantiate will have these methods
+	* Notice the commas after the curly braces at the end of functions
+	* Notice also the commas after method names
+	* What are the other ways to make methods?
+	* This is the same as { foo: "bar", sna: "fu" }, but remember:
+	* In JavaScript, variables point to functions, functions are indexed by variables
 */
+
 Shape.prototype = {
+
+	//displays the vertex using the p5.js shape + vertex
+	display: function() {
+		beginShape();
+		for(var i = 0; i < this.vertexIndex; i ++) {
+			vertex(this.vertices[i].x, this.vertices[i].y);
+		}
+		endShape();
+		this.trackMouse();
+	},
 	
 	//adds a vertex to the shape
 	addVertex: function (x, y) {
@@ -74,16 +106,6 @@ Shape.prototype = {
 	//returns the most recently added vertex
 	lastVertex: function() {
 	    return this.vertices[this.vertexIndex - 1];
-	},
-	
-	//displays the vertex using the p5.js shape + vertex
-	display: function() {
-		beginShape();
-		for(var i = 0; i < this.vertexIndex; i ++) {
-			vertex(this.vertices[i].x, this.vertices[i].y);
-		}
-		endShape();
-		this.trackMouse();
 	},
 	
 	//closes the shape and disallows further editing
@@ -101,6 +123,7 @@ Shape.prototype = {
 		console.log("endShape();")
 	},
 	
+	//"PRIVATE" METHODS
 	
 	//prints a vertex, takes an internal-to-Shape object representation of a vertex:
 	//{x: x, y: y}
